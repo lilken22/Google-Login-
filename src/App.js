72013e8f0172d620f7a8ee980bf5eb329411a985
Okoroha Kennedy <kennedyokoroha22@gmail.com>
2024-08-02 03:34:41 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MyGoogle from './Component/MyGoogle';
 
 function App() {
   return (
+
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<MyGoogle />} />
+        {/* Add more routes here if needed */}
+      </Routes>
     </div>
+  </Router>
+   
   );
 }
 
 export default App;
+
